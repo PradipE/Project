9dee3d20004d6f1feb1fe0d652de534466e84b5d
Pradip Mondal <pradipmondal629548@gmail.com>
2025-08-01 03:29:29 +0530

    Some file

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,9 +1,23 @@
 import Image from "next/image";
-
+import Logo from "../public/images/Logo.jpg";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import StoryWriter from "@/components/ui/StoryWriter";
 export default function Home() {
   return (
-    <main>
-
+    <main className=" flex-1 flex flex-col">
+      <section className="flex-1 grid grid-cols-1 lg:grid-cols-2" >
+      <div className="bg-purple-500 flex flex-col space-y-5 h-1/2 w-1/3 ml-5 p-10 items-center justify-center mb-10 order-1 lg:order-1 ">
+       <Image src={Logo} alt="Logo" />
+        <Button asChild  className=" bg-purple-700 hover:bg-purple-500 lg:text-xl p-3 lg:w-60  ">
+          <Link href="/stories" className="text-sm">Explore Story Library</Link>
+        </Button>
+      </div>
+      <div>
+        <StoryWriter/>
+      </div>
+        
+      </section>
     </main>
   );
 }
---
+++ next-app/components/ui/StoryWriter.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React, { use } from 'react'
+
+function StoryWriter() {
+  return (
+    <div>
+      story
+    </div>
+  )
+}
+
+export default StoryWriter
